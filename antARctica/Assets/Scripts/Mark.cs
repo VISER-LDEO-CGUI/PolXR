4b33aa02e5deae0ed1a98a4fbf03122261fa3fe2
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-11-21 16:52:13 -0500

    Marks while clicking the images.
    Add marks while clicking the images; fix the problem where the shown image is reversed at the back side; add additional lighting to the scene.

---
+++ antARctica/Assets/Scripts/Mark.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mark : MonoBehaviour, IMixedRealityPointerHandler
+{
+    public GameObject MarkObj;
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        MarkObj.SetActive(true);
+        MarkObj.transform.SetParent(this.transform);
+        MarkObj.transform.position = eventData.Pointer.Result.Details.Point;
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+    }
+
+}
---
+++ antARctica/Assets/Scripts/ShowImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ShowImage : MonoBehaviour
+{
+    public Texture content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //content = ...
+        this.GetComponent<Renderer>().material.SetTexture("_MainTex", content);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
