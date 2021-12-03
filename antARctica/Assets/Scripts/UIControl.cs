bc21f2dd927eed0e363eda5266a3235cc9c66ab3
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-12-03 16:36:09 -0500

    Added MarkObj and CMI picks on L870

---
+++ antARctica/Assets/Scripts/UIControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class UIControl : MonoBehaviour
+{
+    public Text txt;
+    public GameObject MarkObj;
+    public bool SaveFile = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //txt.text = MarkObj.transform.localPosition.ToString();
+        float radarx = MarkObj.transform.localPosition.x * MarkObj.transform.parent.gameObject.transform.localScale.x * 10000;
+        Debug.Log(radarx);
+        //Debug.Log(MarkObj.transform.parent.gameObject.transform.localScale.x);
+
+        // Reference https://forum.unity.com/threads/how-to-write-a-file.8864/
+        if (SaveFile)
+        {
+            var sr = File.CreateText("temp.txt");
+            sr.WriteLine(txt.text);
+            sr.Close();
+            SaveFile = false;
+        }
+    }
+}
