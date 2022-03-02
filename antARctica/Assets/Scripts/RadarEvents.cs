281a7de6326a78dec270d06745000574cc9763d3
guosy1998 <32640170+guosy1998@users.noreply.github.com>
2022-03-01 23:28:52 -0500

    Test
    Try to fix the merge conflicts again

---
+++ antARctica/Assets/Scripts/RadarEvents.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarEvents : MonoBehaviour, IMixedRealityPointerHandler
+{
+    public GameObject Menu;
+    public GameObject MarkObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Show the menu and mark and update the variables.
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        // The menu
+        Menu.transform.GetComponent<UpdateSliders>().ResetRadar(this.transform);
+        Menu.transform.position = Vector3.Lerp(eventData.Pointer.Result.Details.Point, Camera.main.transform.position, 0.9f);
+        Menu.SetActive(true);
+
+        // The mark
+        MarkObj.SetActive(true);
+        MarkObj.transform.rotation = this.transform.rotation;
+        MarkObj.transform.SetParent(this.transform);
+        MarkObj.transform.position = eventData.Pointer.Result.Details.Point;
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+    }
+}
