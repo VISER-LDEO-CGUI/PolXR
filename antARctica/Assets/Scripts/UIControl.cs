c977d707cfcc4a1c7ed46637dac67d4c2da2c8e7
guosy1998 <32640170+guosy1998@users.noreply.github.com>
2022-03-01 23:59:13 -0500

    Try to fix the merge conflict
    Try to fix merge

--- antARctica/Assets/Scripts/UIControl.cs
+++ antARctica/Assets/Scripts/UIControl.cs
@@ -9,28 +9,47 @@ public class UIControl : MonoBehaviour
     public Text txt;
     public GameObject MarkObj;
     public bool SaveFile = false;
+    private float yOrigin;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        yOrigin = 1.75f / 5.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //txt.text = MarkObj.transform.localPosition.ToString();
-        float radarx = MarkObj.transform.localPosition.x * MarkObj.transform.parent.gameObject.transform.localScale.x * 10000;
-        Debug.Log(radarx);
-        //Debug.Log(MarkObj.transform.parent.gameObject.transform.localScale.x);
+        float maxX = MarkObj.transform.parent.gameObject.transform.localScale.x * 10000;
+        float maxY = MarkObj.transform.parent.gameObject.transform.localScale.y * 100;
+        float radarX = (MarkObj.transform.localPosition.x + 0.5f) * maxX;
+        float radarY = (MarkObj.transform.localPosition.y - yOrigin) * maxY;
+        string newText = MarkObj.transform.parent.name + ": (" + radarX.ToString() + ", " + radarY.ToString() + ")\nX: " + maxX.ToString() + ", Y: " + maxY.ToString();
+        if (MarkObj.transform.parent.name != "Antarctica")
+            txt.text = newText;
+        else
+            newText = "No selected points.";
 
         // Reference https://forum.unity.com/threads/how-to-write-a-file.8864/
         if (SaveFile)
         {
-            var sr = File.CreateText("temp.txt");
-            sr.WriteLine(txt.text);
-            sr.Close();
+            if (File.Exists("Assets/temp.txt"))
+            {
+                List<string> tempList = new List<string> { newText };
+                File.AppendAllLines("Assets/temp.txt", tempList);
+            }
+            else
+            {
+                var sr = File.CreateText("Assets/temp.txt");
+                sr.WriteLine(txt.text);
+                sr.Close();
+            }
             SaveFile = false;
         }
+    }
+
+    public void WriteCoordinates()
+    {
+        SaveFile = true;
     }
 }
