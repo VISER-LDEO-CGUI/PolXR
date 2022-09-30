dd415301f5ca2a242e88e0c0f838d4a62accd23f
guosy1998 <32640170+guosy1998@users.noreply.github.com>
2022-09-30 16:39:18 -0400

    Add functions
    Add the drawing of the grid to the scene. Need to add labels.
    Add automatic radar image loading, need to be tested.

--- antARctica/Assets/Scripts/CSVReadPlot.cs
+++ antARctica/Assets/Scripts/CSVReadPlot.cs
@@ -15,17 +15,44 @@ public class CSVReadPlot : MonoBehaviour
     public float ColorRange = 10.0f;
     public Transform Parent;
     public Transform RadarImages;
+    public GameObject radarSample;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Load the images here.
+        /*
+        TextAsset radarPos = (TextAsset)Resources.Load("radarPos", typeof(TextAsset));
+        string[] radarDatas = radarPos.text.Split("\n"[0]);
+        Vector3 radarCoord = new Vector3(0f, 0f, 0f);
+        Vector3 radarScale = new Vector3(1.0f, 5.5f, 0.01f);
+
+        for (int i = 1; i < radarDatas.Length - 1; i++)
+        {
+            string[] radarData = radarDatas[i].Split(","[0]);
+            GameObject radarImage = Instantiate(radarSample, RadarImages);
+            radarImage.name = radarData[0];
+
+            // This should be the centroid of the radar.
+            radarCoord.x = float.Parse(radarData[1]);
+            radarCoord.y = float.Parse(radarData[2]);
+            radarCoord.z = float.Parse(radarData[3]);
+
+            // This should be the width and height of the radar.
+            radarScale.x = float.Parse(radarData[4]);
+            //radarScale.y = float.Parse(radarData[5]);
+
+            radarImage.transform.localPosition = radarCoord;
+            radarImage.transform.localScale = radarScale;
+        }
+        */
+
         // Set the default attributes for the particle system.
         var main = PSLine.main;
         main.startLifetime = 86400f;
         main.startSpeed = 0f;
 
         // Load all the files within the directory.
-        // Need to test the file path problem.
         TextAsset[] files = Array.ConvertAll(Resources.LoadAll(fileRoot, typeof(TextAsset)), asset => (TextAsset)asset);
 
         foreach (TextAsset file in files)
@@ -65,7 +92,7 @@ public class CSVReadPlot : MonoBehaviour
             if (x > -9000 & y > -9000 & z > -9000)
             {
                 CSVPoints[inRange].position = new Vector3(x, y, z);
-                // changes color based on height, but currently, heights are too similar
+                // changes color based on height, but currently, heights are too similar, need to modify it if needed.
                 //CSVPoints[inRange].startColor = new Color(PSColor.r, (y - ColorMid) / ColorRange, PSColor.b, 1.0f);
                 inRange += 1;
             }
---
+++ antARctica/Assets/Scripts/DrawGrid.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGrid : MonoBehaviour
+{
+    public GameObject gridLine;
+    public Vector3 startPoint;
+    public int xNum;
+    public int yNum;
+    public int xGap;
+    public int yGap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Draw the grid.
+        Vector3 point0 = startPoint;
+        Vector3 point1 = startPoint;
+        point1.x += (xNum - 1) * xGap;
+        for (int i = 0; i < yNum; i++)
+        {
+            GameObject newLine = Instantiate(gridLine, this.transform);
+            newLine.GetComponent<LineRenderer>().SetPosition(0, point0);
+            newLine.GetComponent<LineRenderer>().SetPosition(1, point1);
+            point0.z += yGap;
+            point1.z += yGap;
+        }
+        
+        point0 = startPoint;
+        point1 = startPoint;
+        point1.z += (yNum - 1) * yGap;
+        for (int i = 0; i < xNum; i++)
+        {
+            GameObject newLine = Instantiate(gridLine, this.transform);
+            newLine.GetComponent<LineRenderer>().SetPosition(0, point0);
+            newLine.GetComponent<LineRenderer>().SetPosition(1, point1);
+            point0.x += xGap;
+            point1.x += xGap;
+        }
+
+        Destroy(gridLine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
