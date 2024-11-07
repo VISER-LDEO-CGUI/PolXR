098527352ceb03d2f3f044f99677595b494fa1c8
LEAAHKIM <lk2935@barnard.edu>
2024-11-06 23:12:28 -0500

    changed selection approach to creating sprites from radargram objects

--- PolXR/Assets/SnapRadargramManager.cs
+++ PolXR/Assets/SnapRadargramManager.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using UnityEngine;
+using UnityEngine.UI;
+
 public class SnapRadargramManager : MonoBehaviour
 {
-    public RectTransform snapPosition1;
-    public RectTransform snapPosition2;
+    public Image snapImage1;  // Reference to the Image component for the first radargram
+    public Image snapImage2;  // Reference to the Image component for the second radargram
 
     private int maxSelection = 2;
     private int currentSelectionIndex = 0;
-
-    private List<GameObject> snappedRadargrams = new List<GameObject>();
-    private Dictionary<GameObject, OriginalTransform> originalTransforms = new Dictionary<GameObject, OriginalTransform>();
 
     public void OnRadargramSelected(GameObject radargram)
     {
@@ -22,74 +22,75 @@ public class SnapRadargramManager : MonoBehaviour
             return;
         }
 
-        if(snappedRadargrams.Contains(radargram))
+        Sprite radargramSprite = ConvertRadargramTextureToSprite(radargram);
+        
+        if (radargramSprite == null)
         {
+            Debug.LogError("Failed to convert radargram to sprite.");
             return;
         }
+
         if (currentSelectionIndex == 0)
         {
-            MoveRadargram(radargram, snapPosition1);
+            snapImage1.sprite = radargramSprite;
+            snapImage1.gameObject.SetActive(true);  // Make sure the image is visible
         }
         else if (currentSelectionIndex == 1)
         {
-            MoveRadargram(radargram, snapPosition2);
+            snapImage2.sprite = radargramSprite;
+            snapImage2.gameObject.SetActive(true);  // Make sure the image is visible
         }
 
-        // Save original transform data to return it later
-        originalTransforms[radargram] = new OriginalTransform(radargram.transform.parent, radargram.transform.position, radargram.transform.rotation, radargram.transform.localScale);
-
-        snappedRadargrams.Add(radargram);
         currentSelectionIndex++;
     }
 
-    private void MoveRadargram(GameObject radargram, RectTransform snapPosition)
+    private Texture2D CreateTextureCopy(Texture2D sourceTexture)
     {
-        radargram.transform.SetParent(snapPosition, false);
+        // Create a new Texture2D with the same dimensions as the source
+        Texture2D textureCopy = new Texture2D(sourceTexture.width, sourceTexture.height, sourceTexture.format, false);
 
-        radargram.transform.localPosition = Vector3.zero;
-        radargram.transform.localRotation = Quaternion.identity;
+        // Copy the pixels from the source texture to the new one
+        textureCopy.SetPixels(sourceTexture.GetPixels());
+        textureCopy.Apply();
 
-        radargram.transform.localScale = Vector3.one;
+        return textureCopy;
+    }
 
-        Debug.Log("Radargram moved to snap position: " + snapPosition.name);
+    private Sprite ConvertRadargramTextureToSprite(GameObject radargram)
+    {
+        MeshRenderer renderer = radargram.GetComponentInChildren<MeshRenderer>();
+
+        if (renderer == null)
+        {
+            Debug.LogError($"Radargram '{radargram.name}' does not have a MeshRenderer component.");
+            return null;
+        }
+
+        Texture2D originalTexture = renderer.material.mainTexture as Texture2D;
+
+        if (originalTexture == null)
+        {
+            Debug.LogError($"Radargram '{radargram.name}' does not have a valid Texture2D assigned.");
+            return null;
+        }
+
+        // Create a unique copy of the texture
+        Texture2D textureCopy = CreateTextureCopy(originalTexture);
+
+        // Create a new sprite from the texture copy
+        return Sprite.Create(textureCopy, new Rect(0, 0, textureCopy.width, textureCopy.height), new Vector2(0.5f, 0.5f));
     }
 
     public void DeselectRadargrams()
     {
         currentSelectionIndex = 0;
 
-        foreach (var radargram in snappedRadargrams)
-        {
-            if (radargram != null && originalTransforms.ContainsKey(radargram))
-            {
-                OriginalTransform originalTransform = originalTransforms[radargram];
-                radargram.transform.SetParent(originalTransform.parent);
-                radargram.transform.position = originalTransform.position;
-                radargram.transform.rotation = originalTransform.rotation;
-                radargram.transform.localScale = originalTransform.localScale;
-            }
-        }
+        // Reset the snap images
+        snapImage1.sprite = null;
+        snapImage1.gameObject.SetActive(false);  // Hide the image
+        snapImage2.sprite = null;
+        snapImage2.gameObject.SetActive(false);  // Hide the image
 
-        snappedRadargrams.Clear();
-        originalTransforms.Clear();
         Debug.Log("Radargrams deselected");
     }
-
-    private class OriginalTransform
-    {
-        public Transform parent;
-        public Vector3 position;
-        public Quaternion rotation;
-        public Vector3 localScale;
-
-        public OriginalTransform(Transform parent, Vector3 position, Quaternion rotation, Vector3 localScale)
-        {
-            this.parent = parent;
-            this.position = position;
-            this.rotation = rotation;
-            this.localScale = localScale;
-        }
-    }
 }
-
-
